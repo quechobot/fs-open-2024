2278e7a210acac650c0d05fa16c302e0a3dcb293
cristopher Quecholac <alum.2c@gmail.com>
2024-01-30 12:25:35 -0600

    exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 const Header = ({text}) => (<h1>{text}</h1>)
 const Button = ({onSmash, text}) => (<button onClick={onSmash}>{text}</button>)
-const Statistics = ({goodNumber, neutralNumber, badNumber}) => {
+const Statistics = ({goodNumber, neutralNumber, badNumber, totalNumber}) => {
+    const avg = totalNumber !== 0 ?  (goodNumber - badNumber)/totalNumber : 0;
+    const positivePer = totalNumber !== 0 ? (goodNumber*100)/totalNumber : 0;
     return(
         <>
             <p>good {goodNumber}</p>
             <p>neutral {neutralNumber}</p>
             <p>bad {badNumber}</p>
+            <p>all {totalNumber}</p>
+            <p>average: {avg}</p>
+            <p>positive {positivePer} %</p>
         </>
     )
 }
@@ -14,9 +19,24 @@ function App() {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
-    const increaseGood = () => setGood(good + 1);
-    const increaseNeutral = () => setNeutral(neutral + 1);
-    const increaseBad = () => setBad(bad + 1);
+    // study the diference/benefits  of useState and a let variable +
+    // let total=good+neutral+bad;
+    const [total, setTotal] = useState(0);
+    const increaseGood = () =>{
+        const updatedGood = good+1;
+        setGood(updatedGood);
+        setTotal(updatedGood+neutral+bad);
+    }
+    const increaseNeutral = () =>{
+        const updatedNeutral = neutral+1;
+        setNeutral(updatedNeutral);
+        setTotal(updatedNeutral+good+bad);
+    }
+    const increaseBad = () =>{
+        const updatedBad = bad+1;
+        setBad(updatedBad);
+        setTotal(updatedBad+good+neutral);
+    }
     return (
         <div>
             <Header text={"give feedback"}/>
@@ -24,7 +44,7 @@ function App() {
             <Button onSmash={increaseNeutral} text='neutral' />
             <Button onSmash={increaseBad} text='bad' />
             <Header text={"statistics"}/>
-            <Statistics goodNumber={good} neutralNumber={neutral} badNumber={bad} />
+            <Statistics goodNumber={good} neutralNumber={neutral} badNumber={bad} totalNumber={total}/>
         </div>
     )
 }
