87d20571cdf5f7698720b97e39d1baa9eaac2ff7
cristopher Quecholac <alum.2c@gmail.com>
2024-01-30 09:55:42 -0600

    exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,31 @@
-
-function App() {
-
-  return (
-      <>
-          <h1>unicafe</h1>
-      </>
-  )
+import { useState } from 'react'
+const Header = ({text}) => (<h1>{text}</h1>)
+const Button = ({onSmash, text}) => (<button onClick={onSmash}>{text}</button>)
+const Statistics = ({goodNumber, neutralNumber, badNumber}) => {
+    return(
+        <>
+            <p>good {goodNumber}</p>
+            <p>neutral {neutralNumber}</p>
+            <p>bad {badNumber}</p>
+        </>
+    )
 }
-
+function App() {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+    const increaseGood = () => setGood(good + 1);
+    const increaseNeutral = () => setNeutral(neutral + 1);
+    const increaseBad = () => setBad(bad + 1);
+    return (
+        <div>
+            <Header text={"give feedback"}/>
+            <Button onSmash={increaseGood} text='good' />
+            <Button onSmash={increaseNeutral} text='neutral' />
+            <Button onSmash={increaseBad} text='bad' />
+            <Header text={"statistics"}/>
+            <Statistics goodNumber={good} neutralNumber={neutral} badNumber={bad} />
+        </div>
+    )
+}
 export default App
