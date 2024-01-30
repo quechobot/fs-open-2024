1f92c4d74f9d441f19f788010f4fa1fafe40451b
cristopher Quecholac <alum.2c@gmail.com>
2024-01-30 12:54:57 -0600

    exercise 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,33 @@
 import { useState } from 'react'
 const Header = ({text}) => (<h1>{text}</h1>)
 const Button = ({onSmash, text}) => (<button onClick={onSmash}>{text}</button>)
+const StatisticLine = ({text, value}) => (<p>{text} {value}</p>)
 const Statistics = ({goodNumber, neutralNumber, badNumber, totalNumber}) => {
     if(totalNumber!==0){
         const avg = (goodNumber - badNumber)/totalNumber;
         const positivePer = (goodNumber*100)/totalNumber;
         return (
-            <>
-                <p>good {goodNumber}</p>
-                <p>neutral {neutralNumber}</p>
-                <p>bad {badNumber}</p>
-                <p>all {totalNumber}</p>
-                <p>average: {avg}</p>
-                <p>positive {positivePer} %</p>
-            </>
-        )
+            <div>
+                <StatisticLine text={"good"} value={goodNumber} />
+                <StatisticLine text={"neutral"} value={neutralNumber} />
+                <StatisticLine text={"bad"} value={badNumber} />
+                <StatisticLine text={"all"} value={totalNumber} />
+                <StatisticLine text={"average"} value={avg} />
+                <StatisticLine text={"positive"} value={positivePer + " %"} />
+            </div>
+        );
     } else {
         return (
             <>
                 <p>No feedback given</p>
             </>
-        )
+        );
     }
 }
 function App() {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
-    // study the diference/benefits  of useState and a let variable +
-    // let total=good+neutral+bad;
     const [total, setTotal] = useState(0);
     const increaseGood = () =>{
         const updatedGood = good+1;
@@ -54,6 +53,6 @@ function App() {
             <Header text={"statistics"}/>
             <Statistics goodNumber={good} neutralNumber={neutral} badNumber={bad} totalNumber={total}/>
         </div>
-    )
+    );
 }
 export default App
