2db5a006d806c5cc4e52a176457b985a67d5d33f
cristopher Quecholac <alum.2c@gmail.com>
2024-02-07 17:09:20 -0600

    exercise 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,10 +1,41 @@
 import { useState } from 'react'
 
+const Filter = ({onType})=>{
+    return(
+        <div>
+            Filter shown with<input onChange={onType}/>
+        </div>
+    );
+}
+const PersonForm = ({onSub, newName, newNumber, onTypeName, onTypeNumber})=>{
+    return(
+        <div>
+            <form onSubmit={onSub}>
+                <div>name: <input value={newName} onChange={onTypeName}/></div>
+                <div>number: <input value={newNumber} onChange={onTypeNumber}/></div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+        </div>
+    );
+}
+const Persons = ({persons}) => {
+    return (
+        <>
+            <ul>
+                {persons.map(person=><Person key={person.id} person={person}/>)}
+            </ul>
+        </>
+    );
+}
+const Person = ({person}) =><li>{person.name} {person.number}</li>
+
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        {name: 'Arto Hellas', number: '040-123456', id: 1},
+        {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
+        {name: 'Dan Abramov', number: '12-43-234345', id: 3 },
         { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
         ]);
     const [newName, setNewName] = useState('');
@@ -37,19 +68,12 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
-            <div>Filter shown with<input onChange={handleFilterChange}/></div>
+            <Filter onType={handleFilterChange} />
             <h2>Add a new</h2>
-            <form onSubmit={addPerson}>
-                <div>name: <input value={newName} onChange={handleNameChange}/></div>
-                <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
-                <div><button type="submit">add</button></div>
-            </form>
+            <PersonForm onSub={addPerson} newName={newName} newNumber={newNumber} onTypeName={handleNameChange} onTypeNumber={handleNumberChange} />
             <h2>Numbers</h2>
-            <ul>
-                {personsToShow.map(person=><li key={person.id}>{person.name} {person.number}</li>)}
-            </ul>
+            <Persons persons={personsToShow}/>
         </div>
     )
 }
-
 export default App
