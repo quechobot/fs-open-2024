e903d8538c140007813cebcbe5333d1781025363
cristopher Quecholac <alum.2c@gmail.com>
2024-02-07 16:51:37 -0600

    exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {
-            name: 'Arto Hellas',
-            number: '040-1234567',
-            id: 1
-        }]);
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+        ]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
+    const [filter, setFilter] = useState('');
+    const personsToShow = persons.filter(e => e.name.split(" ").join("").toLowerCase().includes(filter));
     const addPerson = (event) => {
         event.preventDefault();
         const nameToSearch = newName.split(" ").join("").toLowerCase();
@@ -31,9 +33,12 @@ const App = () => {
     }
     const handleNameChange = event=>setNewName(event.target.value)
     const handleNumberChange = event=>setNewNumber(event.target.value)
+    const handleFilterChange = event=>setFilter(event.target.value.split(" ").join("").toLowerCase())
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>Filter shown with<input onChange={handleFilterChange}/></div>
+            <h2>Add a new</h2>
             <form onSubmit={addPerson}>
                 <div>name: <input value={newName} onChange={handleNameChange}/></div>
                 <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
@@ -41,7 +46,7 @@ const App = () => {
             </form>
             <h2>Numbers</h2>
             <ul>
-                {persons.map(person=><li key={person.id}>{person.name} {person.number}</li>)}
+                {personsToShow.map(person=><li key={person.id}>{person.name} {person.number}</li>)}
             </ul>
         </div>
     )
