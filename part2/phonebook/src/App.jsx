6685926b328245520469b3beba7065de7dd104f9
cristopher Quecholac <alum.2c@gmail.com>
2024-02-08 12:09:48 -0600

    exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({onType})=>{
     return(
@@ -32,16 +33,22 @@ const Persons = ({persons}) => {
 const Person = ({person}) =><li>{person.name} {person.number}</li>
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '040-123456', id: 1},
-        {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
-        {name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-        ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [filter, setFilter] = useState('');
     const personsToShow = persons.filter(e => e.name.split(" ").join("").toLowerCase().includes(filter));
+    const hook = () => {
+        console.log('effect')
+        axios.get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled')
+                setPersons(response.data)
+            })
+    }
+    console.log('render', persons.length, 'persons')
+
+    useEffect(hook, [])
     const addPerson = (event) => {
         event.preventDefault();
         const nameToSearch = newName.split(" ").join("").toLowerCase();
