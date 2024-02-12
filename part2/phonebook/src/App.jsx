3095e7624aa324d6db8325bbc704425535308dcf
cristopher Quecholac <alum.2c@gmail.com>
2024-02-12 16:27:38 -0600

    exercise 2.16-2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -31,12 +31,42 @@ const Persons = ({persons, handleDelete}) => {
     );
 }
 const Person = ({person, handleDelete}) => <li>{person.name} {person.number}<button onClick={()=>handleDelete(person)}>delete</button></li>
+const Notification = ({ message, notificationStyle}) => {
+    if (message === null) {
+        return null
+    }
+    return (
+        <div style={notificationStyle}>
+            {message}
+        </div>
+    )
+}
 const App = () => {
     const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [filter, setFilter] = useState('');
     const personsToShow = persons.filter(e => e.name.split(" ").join("").toLowerCase().includes(filter));
+    const [alertMessage, setAlertMessage] = useState(null);
+    const [notificationStyle, setNotificationStyle] = useState(null);
+    const redNotificationStyle = {
+        color: 'red',
+        background: 'lightgrey',
+        fontSize: 20,
+        borderStyle: 'solid',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10
+    }
+    const greenNotificationStyle = {
+        color: 'green',
+        background: 'lightgrey',
+        fontSize: 20,
+        borderStyle: 'solid',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10
+    }
     const hook = () => {
         personService.getAll()
             .then(initialPersons => {
@@ -58,6 +88,11 @@ const App = () => {
             personService.create(personObject)
                 .then( returnedPerson =>{
                     console.log("created Person in server",returnedPerson);
+                    setNotificationStyle(greenNotificationStyle);
+                    setAlertMessage(`created Person in server '${returnedPerson.name}'`)
+                    setTimeout(() => {
+                        setAlertMessage(null)
+                    }, 3000);
                     setPersons(persons.concat(returnedPerson));
                     setNewNumber('');
                     setNewName('');
@@ -72,8 +107,18 @@ const App = () => {
                         setPersons(persons.map(p=> p.id !== personNewNumber.id ? p:returnedPerson));
                         setNewName("");
                         setNewNumber('');
+                        setNotificationStyle(greenNotificationStyle);
+                        setAlertMessage(`Person '${personNewNumber.name}' updated`)
+                        setTimeout(() => {
+                            setAlertMessage(null)
+                        }, 3000)
                     }).catch(error =>{
-                        alert(`${personNewNumber} was already deleted from the server`);
+                        //alert(`${personNewNumber} was already deleted from the server`);
+                        setNotificationStyle(redNotificationStyle);
+                        setAlertMessage(`Person '${personNewNumber.name}' was already removed from server`)
+                        setTimeout(() => {
+                            setAlertMessage(null)
+                        }, 3000)
                         setPersons(persons.filter(n => n.id !== personFound.id));
                     })
             }
@@ -88,13 +133,27 @@ const App = () => {
             personService.deleteFromServer(person.id)
                 .then(returnedPerson=>{
                     console.log("Deleted person from server", returnedPerson);
+                    setNotificationStyle(redNotificationStyle);
+                    setAlertMessage(`Deleted '${returnedPerson.name}' Person from server `)
+                    setTimeout(() => {
+                        setAlertMessage(null)
+                    }, 3000);
                     setPersons(persons.filter(p => p.id !== returnedPerson.id));
-                })
+                }).catch(error =>{
+                //alert(`${personNewNumber} was already deleted from the server`);
+                    setNotificationStyle(redNotificationStyle);
+                    setAlertMessage(`Person '${person.name}' was already removed from server`)
+                    setTimeout(() => {
+                        setAlertMessage(null)
+                    }, 3000)
+                setPersons(persons.filter(n => n.id !== person.id));
+            })
         }
     }
     return (
         <div>
             <h2>Phonebook</h2>
+            <Notification message={alertMessage} notificationStyle={notificationStyle}/>
             <Filter onType={handleFilterChange} />
             <h2>Add a new</h2>
             <PersonForm onSub={addPerson} newName={newName} newNumber={newNumber} onTypeName={handleNameChange} onTypeNumber={handleNumberChange} />
